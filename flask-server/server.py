3b42ee72ec32ebed8506e2cf98198e5db0a700cf
asd <poo1oox@outlook.com>
2025-08-01 22:19:02 +0900

    update: render init file

--- flask-server/server.py
+++ flask-server/server.py
@@ -3,9 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 # Members API Route
-@app.route("/members")
+# @app.route("/members")
+# def members():
+#     return {"members": ["Member1", "Member2", "Member3"]}
+@app.route("/")
 def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+    return 'hello'
 
 if __name__ == "__main__":
     app.run(debug=True)
