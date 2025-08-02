d28cc58ebca197b152a743aeca8593d36617a283
asd <poo1oox@outlook.com>
2025-08-02 12:55:03 +0900

    Update server.py
    json test

--- flask-server/server.py
+++ flask-server/server.py
@@ -3,12 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 # Members API Route
-# @app.route("/members")
-# def members():
-#     return {"members": ["Member1", "Member2", "Member3"]}
 @app.route("/")
 def members():
-    return 'hello'
+    return {"members": ["Member1", "Member2", "Member3"]}
+# @app.route("/")
+# def members():
+#     return 'hello'
 
 if __name__ == "__main__":
     app.run(debug=True)
